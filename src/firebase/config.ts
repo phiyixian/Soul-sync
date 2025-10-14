fc197096adeecd4fd09528a8af3a1563274e3fd1
Phi Yi Xian <priverco5614@gmail.com>
2025-10-14 07:01:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8158823289-aa6bd",
+  "appId": "1:730510574963:web:c40eabd747626aa3803106",
+  "apiKey": "AIzaSyABsSOezOOrYn6Mtf0TCPoDIiZnWlUexYQ",
+  "authDomain": "studio-8158823289-aa6bd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "730510574963"
+};
